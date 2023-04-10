2fe65f653dd25dc9b84bc934ddafb7d8315e90f6
monukumar98 <monu012as@gmail.com>
2023-04-10 10:36:39 +0530

    Qps

---
+++ Lec7/Pascal_Triangle.java
@@ -0,0 +1,29 @@
+package Lec7;
+
+import java.util.Scanner;
+
+public class Pascal_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int row = 0;
+		int star = 1;
+
+		while (row < n) {
+			int i = 0;
+			int val = 1;
+			while (i < star) {
+				System.out.print(val + " ");
+				val = (val * (row - i)) / (i + 1);
+				i++;
+			}
+			row++;
+			star++;
+			System.out.println();
+		}
+
+	}
+
+}
