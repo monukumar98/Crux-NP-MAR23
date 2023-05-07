cc52c9e41c9ba417de37ca7b25e806a2541d3a0b
monukumar98 <monu012as@gmail.com>
2023-05-07 19:35:27 +0530

    2d Array

---
+++ Lec14/Arrays_2_D.java
@@ -0,0 +1,15 @@
+package Lec14;
+
+public class Arrays_2_D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = new int[3][4];
+		System.out.println(arr);
+		System.out.println(arr[2]);
+		System.out.println(arr[2][1]);
+//		int[][] other = arr;
+
+	}
+
+}
---
+++ Lec14/Matrix_Search.java
@@ -0,0 +1,31 @@
+package Lec14;
+
+public class Matrix_Search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = { { 1, 4, 7, 11, 15 }, { 2, 5, 8, 12, 19 }, { 3, 6, 9, 16, 22 }, { 10, 13, 14, 17, 24 },
+				{ 18, 21, 23, 26, 30 } };
+		int item = 5;
+		System.out.println(Search(arr, item));
+	}
+
+	public static boolean Search(int[][] arr, int item) {
+		// TODO Auto-generated method stub
+		int row = 0;
+		int col = arr[0].length - 1;
+		while (row < arr.length && col >= 0) {
+			if (arr[row][col] == item) {
+				return true;
+			} else if (arr[row][col] > item) {
+				col--;
+			} else {
+				row++;
+			}
+
+		}
+		return false;
+
+	}
+
+}
