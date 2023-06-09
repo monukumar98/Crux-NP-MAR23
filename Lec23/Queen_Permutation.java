5ba60e2b643a4a98872c2733e84e5f79136eaab8
monukumar98 <monu012as@gmail.com>
2023-06-09 12:45:14 +0530

    Backtraking

---
+++ Lec23/Queen_Permutation.java
@@ -0,0 +1,29 @@
+package Lec23;
+
+public class Queen_Permutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean[] board = new boolean[4];
+		int tq = 2;
+		Permutation(board, tq, "", 0);
+
+	}
+
+	public static void Permutation(boolean[] board, int tq, String ans, int qpsf) {
+		if (tq == qpsf) {
+			System.out.println(ans);
+			return;
+		}
+
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == false) {
+				board[i] = true;
+				Permutation(board, tq, ans + "b" + i + "q" + qpsf, qpsf + 1);
+				board[i] = false;
+			}
+		}
+
+	}
+
+}
